369bb0167d8b7af16e99b973c34a746f28288636
Giancarlo Daniele <gdaniele@stanford.edu>
2015-12-12 17:54:02 -0800

    More documentation and api implementation

--- SwiftCamera/CameraController.swift
+++ SwiftCamera/CameraController.swift
@@ -8,10 +8,28 @@ import AVFoundation
 import Photos
 import UIKit
 
-public enum CameraControllerError: ErrorType {
-	case NoVideoDevice
+/*!
+@error CameraControllerVideoDeviceError
+@abstract
+`CameraControllerVideoDeviceError` represents CameraController video device error possibilities.
+
+@discussion
+Camera controller may fail to function due to a variety of setup and permissions errors represented in this enum.
+*/
+public enum CameraControllerVideoDeviceError: ErrorType {
+	case NotAuthorized
+	case NotFound
+	case SetupFailed
 }
 
+/*!
+@extension CameraController
+@abstract
+CameraController provides an easy-to-use api for managing a hardware camera interface in iOS.
+
+@discussion
+`CameraController` provides an interface for setting up and performing common camera functions.
+*/
 public protocol CameraController {
 	// Properties
 	var flashMode: AVCaptureFlashMode { get set }
@@ -22,6 +40,31 @@ public protocol CameraController {
 	func startVideoRecording()
 	func stopVideoRecording()
 	func takePhoto()
+}
+
+/*!
+@extension AVCaptureVideoOrientation
+@abstract
+AVCaptureVideoOrientation is an enum representing the desired orientation of an AVFoundation capture.
+
+@discussion
+`fromUIInterfaceOrientation()` allows easy conversion from the enum representing the current iOS device's UI and the enum representing AVFoundation orientation possibilities.
+*/
+extension AVCaptureVideoOrientation {
+	static func fromUIInterfaceOrientation(orientation: UIInterfaceOrientation) throws -> AVCaptureVideoOrientation {
+		switch orientation {
+		case .Portrait:
+			return .Portrait
+		case .PortraitUpsideDown:
+			return .PortraitUpsideDown
+		case .LandscapeRight:
+			return .LandscapeRight
+		case .LandscapeLeft:
+			return .LandscapeLeft
+		default:
+			throw ErrorType
+		}
+	}
 }
 
 /*!
@@ -40,6 +83,7 @@ public enum CameraControllerSetupResult {
 	case ConfigurationFailed
 	case NotAuthorized
 	case Success
+	case Restricted
 }
 
 /*!
@@ -52,9 +96,11 @@ An AVFoundationCameraController uses the AVFoundation framework to manage a came
 */
 public class AVFoundationCameraController: NSObject, CameraController {
 	// Session management
+	private let movieFileOutput: AVCaptureMovieFileOutput? = nil
 	private let session: AVCaptureSession
 	private let sessionQueue: dispatch_queue_t
 	private let stillImageOutput: AVCaptureStillImageOutput? = nil
+	private var videoDeviceInput: AVCaptureDeviceInput? = nil
 	
 	// State
 	private let captureFlashMode: AVCaptureFlashMode = .Off
@@ -63,7 +109,7 @@ public class AVFoundationCameraController: NSObject, CameraController {
 	private var backgroundRecordingID: UIBackgroundTaskIdentifier = UIBackgroundTaskInvalid
 	private var sessionRunning: Bool = false
 
-	init(configurationFailedErrorBlock: (()->())?, notAuthorizedErrorBlock: (()-> ())?) {
+	init(configurationFailedErrorBlock: ((CameraControllerVideoDeviceError)->())?, notAuthorizedErrorBlock: ((CameraControllerSetupResult)-> ())?) {
 		// Create session
 		self.session = AVCaptureSession()
 		self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL)
@@ -74,7 +120,9 @@ public class AVFoundationCameraController: NSObject, CameraController {
 		self.checkAuthorizationStatus(notAuthorizedErrorBlock)
 		
 		// Set up the capture session
-		self.setCaptureSession()
+		self.setCaptureSession { error in
+			configurationFailedErrorBlock?(error)
+		}
 	}
 	
 	// MARK:- Public Properties
@@ -107,7 +155,7 @@ public class AVFoundationCameraController: NSObject, CameraController {
 		guard let uPreferredDevice = (preferredDevice as? AVCaptureDevice) where preferredDevice is AVCaptureDevice else {
 			
 			guard let uDefaultDevice = (defaultDevice as? AVCaptureDevice) where defaultDevice is AVCaptureDevice else {
-				throw CameraControllerError.NoVideoDevice
+				throw CameraControllerVideoDeviceError.NotFound
 			}
 			
 			return uDefaultDevice
@@ -141,29 +189,30 @@ public class AVFoundationCameraController: NSObject, CameraController {
 	// MARK:- Private API
 	
 	// Checks video authorization status and updates `setupResult`. Note: audio authorization will be requested automatically when an AVCaptureDeviceInput is created.
-	private func checkAuthorizationStatus(notAuthorizedErrorBlock: (()-> ())?) {
+	private func checkAuthorizationStatus(notAuthorizedErrorBlock: ((CameraControllerSetupResult)-> ())?) {
 		switch AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo) {
 		case .NotDetermined:
 			self.requestAccess(notAuthorizedErrorBlock)
 		case .Denied:
 			self.setupResult = .NotAuthorized
-			notAuthorizedErrorBlock?()
+			notAuthorizedErrorBlock?(.NotAuthorized)
 		case .Restricted:
 			self.setupResult = .ConfigurationFailed
+			notAuthorizedErrorBlock?(.Restricted)
 			print("Access to the media device is restricted")
 		default: break
 		}
 	}
 	
 	// Gives user the option to grant video access. Suspends the session queue to avoid asking the user for audio access (via session queue initialization) if video access has not yet been granted.
-	private func requestAccess(notAuthorizedErrorBlock: (()-> ())?) {
+	private func requestAccess(notAuthorizedErrorBlock: ((CameraControllerSetupResult)-> ())?) {
 		dispatch_suspend(self.sessionQueue)
 		
 		AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: { granted in
 			dispatch_resume(self.sessionQueue)
 
 			guard granted else {
-				notAuthorizedErrorBlock?()
+				notAuthorizedErrorBlock?(.NotAuthorized)
 				self.setupResult = .NotAuthorized
 				return
 			}
@@ -171,15 +220,48 @@ public class AVFoundationCameraController: NSObject, CameraController {
 	}
 	
 	// Sets up the capture session. Note: AVCaptureSession.startRunning is synchronous and might take a while to execute. For this reason, we start the session on the coordinated shared `sessionQueue` (and use that queue to access the camera in any further actions)
-	private func setCaptureSession() {
+	private func setCaptureSession(error: ((CameraControllerVideoDeviceError) -> ())?) {
 		dispatch_async(self.sessionQueue, { () in
 			guard self.setupResult == .Success else {
+				error?(CameraControllerVideoDeviceError.NotAuthorized)
 				return
 			}
 			
 			self.backgroundRecordingID = UIBackgroundTaskInvalid
-
 			
+			guard let videoDevice = try? AVFoundationCameraController.deviceWithMediaType(AVMediaTypeVideo, preferredPosition: AVCaptureDevicePosition.Back), let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else {
+				print("Could not get video device")
+				error?(CameraControllerVideoDeviceError.SetupFailed)
+				return
+			}
+			
+			self.session.beginConfiguration()
+			
+			guard self.session.canAddInput(videoDeviceInput) else {
+				error?(CameraControllerVideoDeviceError.SetupFailed)
+				print("Could not add video device input to the session")
+				return
+			}
+			
+			self.session.addInput(videoDeviceInput)
+			self.videoDeviceInput = videoDeviceInput
+			dispatch_async(dispatch_get_main_queue(), {
+				// We need to dispatch to the main thread here
+				// because our preview layer is backed by UIKit
+				// which runs on the main thread
+				let currentStatusBarOrientation = UIApplication.sharedApplication().statusBarOrientation
+				var initialVideoOrientation = AVCaptureVideoOrientation.Portrait
+				
+				guard currentStatusBarOrientation != .Unknown else {
+					return
+				}
+				
+				initialVideoOrientation = AVCaptureVideoOrientation
+				
+				
+			})
+			
+
 		})
 	}
 	
