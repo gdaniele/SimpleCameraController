b488981bb28f2a4bed236e66b8a534c0f5c48321
Giancarlo Daniele <gdaniele@stanford.edu>
2016-05-30 08:59:51 -0700

    Authorize AV on main thread

--- SimpleCameraController/SimpleCameraController/Source/AVAuthorizer.swift
+++ SimpleCameraController/SimpleCameraController/Source/AVAuthorizer.swift
@@ -8,18 +8,26 @@
 
 import AVFoundation
 
+typealias AuthorizationBlock = ((Bool) -> ())?
+
 protocol Authorizer {
   static var videoStatus: AVAuthorizationStatus { get }
   static var audioStatus: AVAuthorizationStatus { get }
   static var supportsFrontCamera: Bool { get }
   static var supportsFlash: Bool { get }
 
-  static func requestAccessForAudio(completion: ((Bool) -> ())?)
-  static func requestAccessForVideo(completion: ((Bool) -> ())?)
+  static func requestAccessForAudio(completion: AuthorizationBlock)
+  static func requestAccessForVideo(completion: AuthorizationBlock)
 }
 
 struct AVAuthorizer: Authorizer {
   private static let camera: Camera.Type = AVCamera.self
+  private static let authorizationBlock: (granted: Bool, block: AuthorizationBlock) -> ()
+    = { granted, block in
+      dispatch_async(dispatch_get_main_queue(), {
+        block?(granted)
+      })
+  }
 
   static var audioStatus: AVAuthorizationStatus {
     return AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeAudio)
@@ -39,11 +47,15 @@ struct AVAuthorizer: Authorizer {
 
   static func requestAccessForAudio(completion: ((Bool) -> ())?) {
     AVCaptureDevice.requestAccessForMediaType(AVMediaTypeAudio,
-                                              completionHandler: { completion?($0) })
+                                              completionHandler: {
+                                                authorizationBlock(granted: $0, block: completion)
+    })
   }
 
   static func requestAccessForVideo(completion: ((Bool) -> ())?) {
     AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo,
-                                              completionHandler: { completion?($0) })
+                                              completionHandler: {
+                                                authorizationBlock(granted: $0, block: completion)
+    })
   }
 }
